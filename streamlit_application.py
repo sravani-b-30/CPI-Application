a64748db146bed8e2920ac1793000f2ef3cc8d4b
B.Sravani <sravani@napqueens.com>
2024-09-20 18:47:27 +0530

    Changed price data to Category Daily Serp Data File of 30Dyas

--- streamlit_application.py
+++ streamlit_application.py
@@ -278,7 +278,7 @@ def load_and_preprocess_data():
     df_merged_cleaned = df_merged_cleaned.drop('asin', axis=1)
 
     # Load additional dataset for time-series analysis
-    df2 = pd.read_csv("AMZ_MATTRESS_Fourth_Sept.csv")
+    df2 = pd.read_csv("AMZ_CDS_MATTRESS.csv")
     # df2 = df2.drop_duplicates(subset=['asin'], keep='first')
     df2['asin'] = df2['asin'].str.upper()
     df_merged_cleaned['ASIN'] = df_merged_cleaned['ASIN'].str.upper()
